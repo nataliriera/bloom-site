7cfebe5fa24cb45494575a6506b26398d66e34c7
DanRie19 <67808802+nataliriera@users.noreply.github.com>
2026-02-06 20:24:53 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { sitemapPlugin } from "vite-plugin-sitemap";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
     react(),
-    sitemapPlugin({
+    sitemap({
       hostname: "https://bloomflowerwallrentals.com",
-      routes: [
+      dynamicRoutes: [
         "/",
         "/gallery",
         "/pricing",
