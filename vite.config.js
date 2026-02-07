bf32505f9ca4c090ffdf37cb268d3ee578ee7667
DanRie19 <67808802+nataliriera@users.noreply.github.com>
2026-02-06 20:23:01 -0500

    f

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { sitemapPlugin } from "vite-plugin-sitemap";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sitemapPlugin({
+      hostname: "https://bloomflowerwallrentals.com",
+      routes: [
+        "/",
+        "/gallery",
+        "/pricing",
+        "/faq",
+        "/service-areas",
+        "/contact",
+      ],
+    }),
+  ],
+});
